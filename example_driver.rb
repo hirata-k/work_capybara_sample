4cd00971e6d8379c19af8c1a38816ce584345a2e
Hirata Kozo <hirata.k@shanon.co.jp>
2022-06-28 16:01:32 +0900

    fix example_driver

--- example_driver.rb
+++ example_driver.rb
@@ -1,12 +1,14 @@
 require "selenium/webdriver"
 require 'capybara/rspec'
 
+DRIVER = :edge
+
 Capybara.configure do |config|
   # 参照: http://www.rubydoc.info/github/jnicklas/capybara/Capybara.configure
 
   # DSL Options:
-  config.default_driver = :chrome
-  config.javascript_driver = :chrome
+  config.default_driver = DRIVER
+  config.javascript_driver = DRIVER
 
   # Configurable options:
   config.run_server = false            # ローカルのRack Serverを使用しない (Default: true)
@@ -17,42 +19,50 @@ Capybara.configure do |config|
   config.automatic_label_click = false # チェックボックスやラジオボタンが非表示の場合に関連するラベル要素をクリックするかどうか (Default: false)
 end
 
-Capybara.register_driver :chrome do |app|
+    def browser
+      (ENV['browser'] || 'chrome').to_sym
+    end
 
-  # 参考: https://seleniumhq.github.io/selenium/docs/api/rb/Selenium/WebDriver/Chrome/Options.html
-  # 参考: https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings
+    def options
+      browser_str = browser.to_s
+      self.send("#{browser_str}_options")
+    end
 
-  options = Selenium::WebDriver::Chrome::Options.new
+    def chrome_options
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('start-maximized')
+      options.add_argument('disable-infobars')
+      options.add_argument('disable-gpu')
+      options.add_argument('privileged')
+      options.add_argument('ignore-certificate-errors')
+      options.add_argument('no-default-browser-check')
+      options
+    end
 
-  # セキュリティに影響を及ぼす設定の変更は、アクセス先が安全であることを確認してから行なって下さい。
-  # options.add_argument('disable-web-security')      # CORS（Cross-Origin Resource Sharing）を無視する
-  # options.add_argument('ignore-certificate-errors') # SSLサーバ証明書のエラーを無視する
-  # options.add_argument('disable-popup-blocking')    # ポップアップブロックを無効にする
+    def edge_options
+      options = Selenium::WebDriver::Edge::Options.new
+      options.add_argument('start-maximized')
+      options.add_argument('disable-infobars')
+      options.add_argument('disable-gpu')
+      options.add_argument('privileged')
+      options.add_argument('ignore-certificate-errors')
+      options.add_argument('no-default-browser-check')
+      options
+    end
 
-  options.add_argument('disable-notifications')       # Web通知やPush APIによる通知を無視する
-  options.add_argument('disable-translate')           # 翻訳ツールバーを無効にする
-  options.add_argument('disable-extensions')          # 拡張機能を無効にする
-  options.add_argument('disable-infobars')            # インフォバーの表示を無効にする
-  options.add_argument('window-size=1280,960')        # ブラウザーのサイズを指定する
-  # options.add_argument('user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"') # IE6
-  # options.add_argument('headless') # Headlessモードを有効にする
+    def firefox_options
+      options = Selenium::WebDriver::Firefox::Options.new
+      options
+    end
 
-  # ChromeのSPエミュレーションを使用する
-  # options.add_emulation(device_name: 'Nexus 5X') # Device Toolbarで選択出来るEmulated Devicesを指定する
+    def caps
+      browser_str = browser.to_s
+      caps = Selenium::WebDriver::Remote::Capabilities.send(browser_str)
+      caps
+    end
 
-  # ダウンロードファイルの保存場所を指定する
-  # download_dir = File.join(Dir.pwd, 'files/downloads')
-  # FileUtils.mkdir_p(download_dir)
-  # prefs = { prompt_for_download: false, default_directory: download_dir }
-  # options.add_preference(:download, prefs)
+Capybara.register_driver DRIVER do |app|
 
-  # Consoleログを取得出来るようにする
-  # options.add_preference(:loggingPrefs, browser: 'ALL')
-
-  # ChromeやFirefoxの実行パスを明示的に指定する
-  # options.add_option(:binary, '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary')
-
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome
   endpoint = 'http://localhost:4444/wd/hub'
 
   # ブラウザーを起動する
